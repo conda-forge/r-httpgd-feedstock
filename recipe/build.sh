0926946d8562fd9c0b7e554c8a44a3321940010a
Devon Ryan <dpryan79@users.noreply.github.com>
2022-10-20 09:34:52 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
+# https://github.com/nx10/httpgd/commit/efc56d4b6510438842ea36e3ee987a046671ff05
+sed -i.bak "s/CXX17/CXX14/g" src/Makevars.in
 ${R} CMD INSTALL --build . ${R_ARGS}
