f5ee26eabc4affa00e7adf9d0248d6e967adb942
Marcel Bargull <marcel.bargull@udo.edu>
2022-10-21 12:11:51 +0200

    osx-64: Try appending PKG_CXXFLAGS to Makevars.in

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ case ${target_platform} in osx-64 )
   # std::aligned_alloc only available from macOS SDK version 10.15 onward.
   # Boost <=1.79 erroneously wants to use std::aligned_alloc in older SDKs, too.
   # https://www.boost.org/doc/libs/1_80_0/doc/html/boost_asio/history.html#boost_asio.history.asio_1_24_0___boost_1_80
-  export CXXFLAGS="${CXXFLAGS} -DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC"
+  >> src/Makevars.in printf '%s\n' \
+    'PKG_CXXFLAGS += -DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC'
 esac
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
